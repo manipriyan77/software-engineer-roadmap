62066994ffa1d90ed9c34d44ca9576d75925c6d5
manipriyan77 <manipriyangopalan@gmail.com>
2025-02-02 22:22:33 +0530

    added get,set,unshift  to singlyLinkedList

--- javascript-dsa/signly-linked-list/script.js
+++ javascript-dsa/signly-linked-list/script.js
@@ -1,3 +1,5 @@
+const { current } = require('@reduxjs/toolkit');
+
 class Node {
   constructor(val) {
     this.val = val;
@@ -23,10 +25,6 @@ class SinglyLinkedList {
       this.tail = newNode;
     }
     this.length++;
-    if (this.length === 0) {
-      this.head = null;
-      this.tail = null;
-    }
     return this;
   }
 
@@ -35,7 +33,6 @@ class SinglyLinkedList {
     if (!this.head) {
       return null;
     }
-    debugger;
     let current = this.head;
     let newTail = current;
     while (current.next) {
@@ -45,6 +42,10 @@ class SinglyLinkedList {
     this.tail = newTail;
     this.tail.next = null;
     this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
     return current;
   }
 
@@ -55,14 +56,50 @@ class SinglyLinkedList {
     }
     let tempHead = this.head;
     this.head = this.head.next;
-    console.log(this, 'this shift');
+    tempHead.next = null;
     this.length--;
-    if (this.length === 1) {
+    if (this.length === 0) {
       this.tail = null;
     }
     return tempHead;
   }
 
+  // add item on the first / head
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  // get item by index
+  get(index) {
+    let count = 0;
+    let current = this.head;
+    if (index > this.length - 1) {
+      return undefined;
+    }
+    while (index !== count) {
+      current = current.next;
+      count++;
+    }
+    return current;
+  }
+
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
   traverse() {
     let current = this.head;
     while (current) {
@@ -76,7 +113,6 @@ let newSinglyLinkedList = new SinglyLinkedList();
 newSinglyLinkedList.push('Hi');
 
 newSinglyLinkedList.push('Hello');
-newSinglyLinkedList.push('Hello 2');
 
 console.log(newSinglyLinkedList, 'push');
 
@@ -84,3 +120,7 @@ console.log(newSinglyLinkedList, 'push');
 // newSinglyLinkedList.pop();
 // newSinglyLinkedList.pop();
 newSinglyLinkedList.shift();
+newSinglyLinkedList.push('Hello 2');
+
+newSinglyLinkedList.unshift('Value');
+console.log(newSinglyLinkedList, 'unsift');
