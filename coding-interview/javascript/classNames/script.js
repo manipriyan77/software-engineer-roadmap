ba3b36daeda1e66649a093761a07ae5657bc7417
manipriyan77 <manipriyangopalan@gmail.com>
2025-02-11 15:01:59 +0530

    solved some interview questions

---
+++ coding-interview/javascript/classNames/script.js
@@ -0,0 +1,24 @@
+function classNames(...args) {
+  let result = [];
+  function processItem(item) {
+    if (!item) return; // Skip falsy values like null, undefined, false
+
+    if (Array.isArray(item)) {
+      item.forEach(processItem); // Recursively process arrays
+    } else if (typeof item === 'object') {
+      for (let key in item) {
+        if (item[key]) result.push(key); // Only push truthy keys
+      }
+    } else {
+      result.push(item);
+    }
+  }
+
+  args.forEach(processItem);
+  console.log('result :>> ', result);
+  return result.join(' ');
+}
+
+// classNames('foo', { bar: true });
+// classNames(['foo', 'bar', 'baz']);
+classNames(null, false, 'bar', undefined, 0, 1, { baz: null }, '');
