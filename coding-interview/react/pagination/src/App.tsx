11de269f23fab9cf8ee562a6f38a245330ab7204
Manipriyan <manipriyangopalan@gmail.com>
2025-03-20 19:43:28 +0530

    pagination react interview question

--- coding-interview/react/pagination/src/App.tsx
+++ coding-interview/react/pagination/src/App.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
 import './App.css';
 import StarRating from './star-rating';
+import CircleIntersection from './circle-intersection';
+import VsCodeSidebar from './vs-code-sidebar';
 
 function App() {
   const [rating, setRating] = useState(0);
 
   return (
     <>
-      <StarRating rating={rating} setRating={setRating} />
+      {/* <StarRating rating={rating} setRating={setRating} /> */}
+      {/* <CircleIntersection /> */}
+      <VsCodeSidebar />
     </>
   );
 }
