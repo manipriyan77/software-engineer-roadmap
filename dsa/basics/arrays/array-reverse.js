fe8e5ce1bd30204cfb7dd6e133809f8017f603c8
Manipriyan <manipriyangopalan@gmail.com>
2025-07-03 10:48:57 +0530

    cleaned the files

--- dsa/basics/arrays/array-reverse.js
+++
@@ -1,9 +0,0 @@
-function arrayReverse(arr) {
-  let newArray = [];
-  for (let i = arr.length - 1; i >= 0; i--) {
-    newArray.push(arr[i]);
-  }
-  return newArray;
-}
-
-arrayReverse([1, 2, 3, 4, 5]);
---
+++ javascript/behind-the-scenes/js-engine-runtime/readme.md
@@ -0,0 +1,3 @@
+All JS Engine Contains a Callstacck and a Heap
+
+Callstack is where all code is executed using execution context
